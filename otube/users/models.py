5a99fd8e5e3989b37fa38462b41f12f6d0daf9a3
Bounty2049 <icop2049@gmail.com>
2024-04-06 22:10:36 +0400

    update model User

--- otube/users/models.py
+++ otube/users/models.py
@@ -3,12 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=100, blank=False, null=False)
-    last_name = models.CharField(max_length=100, blank=False, null=False)
-    email = models.EmailField(max_length=100, blank=False, null=False)
     image = models.ImageField(upload_to='users_images', null=True, blank=True)
 
-    REQUIRED_FIELDS = (first_name, last_name, email, image)
 
 
 
