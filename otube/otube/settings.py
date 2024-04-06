8756ce666c6ef590e586347de68449ea508e9a07
Bounty2049 <icop2049@gmail.com>
2024-04-06 22:07:38 +0400

    plugged in postgresql database

--- otube/otube/settings.py
+++ otube/otube/settings.py
@@ -81,9 +81,13 @@ WSGI_APPLICATION = 'otube.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'otube_db',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
