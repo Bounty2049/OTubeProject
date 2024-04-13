ca8fe6751ae9fd5fc3382955f3666a541daf3216
ghoul <icorp2049@gmail.com>
2024-04-13 22:46:24 +0400

    make Dockerfile

--- otube/otube/settings.py
+++ otube/otube/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-xbu2ptgz*nhg%ig@=j8mdp$9lgz0$48aoq4x=#bkej0&q2i*jr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0']
 
 
 # Application definition
